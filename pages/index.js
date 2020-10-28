fdbf98487e97de582618eda2a54e15cb1894c6a2
fmvilas <fmvilas@gmail.com>
2020-10-28 16:58:11 +0100

    Remove getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,3 @@ export default function Home() {
     </div>
   )
 }
-
-export async function getServerSideProps(context) {
-  return {
-    props: {}, // will be passed to the page component as props
-  }
-}
