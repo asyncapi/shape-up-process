ead2648b2928cf52b83d52b7d55879a67d44f39a
fmvilas <fmvilas@gmail.com>
2020-10-28 16:47:06 +0100

    Initial work for the dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+  },
+  variants: {},
+  plugins: [],
+}
