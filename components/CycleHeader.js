17c588d4b09fa047b4f09858a5671c7e55c1f805
fmvilas <fmvilas@gmail.com>
2020-11-07 19:21:37 +0100

    Link cycle title to milestone on Github

--- components/CycleHeader.js
+++ components/CycleHeader.js
@@ -5,7 +5,7 @@ export default function CycleHeader({ visibleCycle, inCycle, isPastCycle, previo
     <>
       <div className="flex">
         <h2 className="flex-1 text-2xl leading-6 font-medium text-gray-900">
-          <span>{visibleCycle.title}</span> {inCycle && (<span className="inline-block transform -translate-y-1.5 ml-2 px-2 py-1 text-teal-800 uppercase text-xs leading-4 font-medium border border-teal-100 rounded-full"> <span className="inline-block mr-1 animate-pulse bg-red-500 rounded-full w-2 h-2"></span> Current cycle</span>)}
+          <a href={visibleCycle.html_url} target="_blank">{visibleCycle.title}</a> {inCycle && (<span className="inline-block transform -translate-y-1.5 ml-2 px-2 py-1 text-teal-800 uppercase text-xs leading-4 font-medium border border-teal-100 rounded-full"> <span className="inline-block mr-1 animate-pulse bg-red-500 rounded-full w-2 h-2"></span> Current cycle</span>)}
         </h2>
         <div className="flex">
           <Link href={previousCycle ? `/cycles/${previousCycle.id}` : ''}>
